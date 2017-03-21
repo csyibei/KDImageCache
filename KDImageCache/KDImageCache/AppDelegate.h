52dc95262ffc056eafc618e3a7d0ec44b0385a8b
csyibei <csyibei_120@163.com>
2017-03-21 18:42:25 +0800

    creat new project

---
+++ KDImageCache/KDImageCache/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KDImageCache
+//
+//  Created by laikidi on 17/3/21.
+//  Copyright © 2017年 KiDiL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ KDImageCache/KDImageCache/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KDImageCache
+//
+//  Created by laikidi on 17/3/21.
+//  Copyright © 2017年 KiDiL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
